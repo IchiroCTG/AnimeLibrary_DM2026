7f70cb9db1e5339fed8fa8c062bcfa8316c30daa
Fnav63 <fnavarretecortes63@gmail.com>
2026-06-28 19:15:50 -0400

    feat: AuthViewModel para autenticación Firebase

---
+++ lib/viewmodels/auth_viewmodel.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthViewModel extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? get currentUser => _auth.currentUser;
+
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  Future<String?> signIn(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    } catch (_) {
+      return 'Error desconocido';
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
